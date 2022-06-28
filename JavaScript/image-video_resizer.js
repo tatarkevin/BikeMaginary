927941dd4b16052a4da6e37d43644685c3c07e4a
tatarkevin <kevintatar@web.de>
2022-06-28 21:05:29 +0200

    Safety Commit. Projekt etwas aufgeräumt

---
+++ JavaScript/image-video_resizer.js
@@ -0,0 +1,11 @@
+import ImageKit from "imagekit-javascript";
+
+var imagekit = new ImageKit({
+  urlEndpoint: "https://ik.imagekit.io/4t4ujsvqh/",
+});
+
+var imagekit = new ImageKit({
+  publicKey: "your_public_api_key",
+  urlEndpoint: "https://ik.imagekit.io/4t4ujsvqh/",
+  authenticationEndpoint: "http://www.yourserver.com/auth",
+});
